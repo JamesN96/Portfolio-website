4c54faf15dfc8bcad8871ff16665b561d6aef268
James Nguyen <james.nguyenn96@gmail.com>
2019-10-22 21:59:08 +1100

    Select DOM elements. Set a state for the menu. Add an event listener to the menuBtn and call a function named showMenu. Create the function showMenu

---
+++ dist/js/main.js
@@ -0,0 +1,33 @@
+// DOM CACHE
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBrand = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItem = document.querySelectorAll(".nav-item");
+
+// Menu State
+
+let menuState = false;
+
+menuBtn.addEventListener("click", showMenu);
+
+// showMenu function
+
+function showMenu() {
+  if (!menuState) {
+    menuBtn.classList.add(".close");
+    menu.classList.add(".show");
+    menuBrand.classList.add(".show");
+    menuNav.classList.add(".show");
+    navItem.forEach(item => item.classList.add(".show"));
+    menuState = true;
+  } else {
+    menuBtn.classList.remove(".close");
+    menu.classList.remove(".show");
+    menuBrand.classList.remove(".show");
+    menuNav.classList.remove(".show");
+    navItem.forEach(item => item.classList.remove(".show"));
+    menuState = false;
+  }
+}
