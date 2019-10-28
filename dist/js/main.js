f03e3ab939180f9fc587b69e09ab26c32c4cc3c1
James Nguyen <james.nguyenn96@gmail.com>
2019-10-28 20:20:50 +1100

    Remove some comments

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,6 @@
 
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
-// const menuBrand = document.querySelector(".menu-branding");
 const menuNav = document.querySelector(".menu-nav");
 const navItem = document.querySelectorAll(".nav-item");
 const homeBtn = document.querySelector("#get-started");
@@ -22,14 +21,12 @@ function showMenu() {
   if (!menuState) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
-    // menuBrand.classList.add("show");
     menuNav.classList.add("show");
     navItem.forEach(item => item.classList.add("show"));
     menuState = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
-    // menuBrand.classList.remove("show");
     menuNav.classList.remove("show");
     navItem.forEach(item => item.classList.remove("show"));
     menuState = false;
